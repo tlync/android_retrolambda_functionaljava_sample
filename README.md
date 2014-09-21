95932002589f6684cafb4396e7381cafb63d3f91
daneko <3a3k.web+github@gmail.com>
2014-09-22 02:10:01 +0900

    first

---
+++ README.md
@@ -0,0 +1,1 @@
+### sample retrolambda + functionaljava + android
---
+++ app/src/main/java/com/example/sample/MyActivity.java
@@ -0,0 +1,75 @@
+package com.example.sample;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import lombok.val;
+
+import static fj.data.List.list;
+
+public class MyActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.my, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_my, container, false);
+
+            val sum = list(1, 2, 3, 4).
+                    filter(i -> i % 2 == 0).
+                    map(i -> i * 2).
+                    foldLeft(x -> y -> x + y, 0);
+
+            rootView.findViewById(R.id.button1).
+                    setOnClickListener(v ->
+                                    Toast.makeText(getActivity(), String.format("sum:%d", sum), Toast.LENGTH_LONG).show()
+                    );
+            return rootView;
+        }
+    }
+}
